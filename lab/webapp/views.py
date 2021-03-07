bd81961b49cd1f77ed353bd4dcf3bdee4a86b5af
German <coolmatoff@gmail.com>
2021-03-07 17:17:18 +0600

    Addeded main page

--- lab/webapp/views.py
+++ lab/webapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from webapp.models import Product
 
 # Create your views here.
+
+def index_view(request):
+    products = Product.objects.all().order_by('product_name', 'category')
+    return render(request, 'index.html', context={'products': products})
